3128e2db57feac2cbb6e8def5345510475af501e
QichenJ <QichenJ@JingQichens-MacBook-Pro.local>
2015-10-29 22:45:02 -0700

    basic function works

--- js/views/invView.js
+++ js/views/invView.js
@@ -3,6 +3,7 @@ app.invView = Backbone.View.extend({
 
 	tagName: 'li',
 	template: _.template( $('#item-template').html() ),
+	intTemplate: _.template( $('#ins-select-template').html() ),
 	events: {
 
 	},
@@ -13,6 +14,18 @@ app.invView = Backbone.View.extend({
 
 	render : function() {
 		this.$el.html( this.template( this.model.attributes ));
+		var models = app.Sels;
+		var self = this;
+		var htmlTitle = '<div class="sel-title">'+
+		'<p class="inv-sel">Name</p>' +
+		'<p class="inv-sel">Price</p>' +
+		'<p class="inv-sel">Number</p>'+
+		'<p class="inv-sel">Total</p>'+
+		'<p class="clear"></p></div>';
+		self.$el.append(htmlTitle);
+		models.each(function(model) {
+			self.$el.append(self.intTemplate(model.attributes));
+		});
 		return this;
 	}
 });
